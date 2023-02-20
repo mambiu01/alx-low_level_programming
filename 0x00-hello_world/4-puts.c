ddf1925f9abcf215436e104ac9e1a7a56a45e084
Mambiu01 <mambiu01@gmail.com>
2023-02-20 14:19:57 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-#include <stdiob.h>
+#include <stdio.h>
+/*
+ *  main - Entry point
+ *
+ * Return:Always 0 (success)
+ */
 
 int main(void)
 {
-	put (programming is like a multilingual puzzle\n");
+	put ("programming is like biulding a multilingual puzzle\n")
 	return (0);
 }
 
