804cd147ebaa4a69cf02539a4f010e80300a0914
Mambiu01 <mambiu01@gmail.com>
2023-03-02 14:04:40 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_topper - change all lowercase to uppercase
+ * @n: pointer
+ * Return:n
+ */
+char *string_toupper(char *n)
+{
+	int m;
+
+	m = 0;
+	while (n[m] != '\0')
+	{
+		if (n[m] >= 'a' && n[m] <= 'z')
+			n[m] = n[m] - 32;
+
+		m++;
+	}
+
+	return (n);
+}
