4ccf38baca24b97beaa63d91e28c3f4d51f80180
Mambiu01 <mambiu01@gmail.com>
2023-03-07 12:39:55 -0800

    main files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _meme - a block of memory with specific value
+ * @s: starting address of memory
+ * @b: the value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value of n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+
+{	int a = 0;
+	for (; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
+	return (s) ;
+}
+
